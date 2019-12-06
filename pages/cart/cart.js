f035eb1f0cc82776ad0587520c4da2ace51a5d33
ninefeetunder <1285499453@qq.com>
2019-12-06 22:21:23 +0800

    详情页完成

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -1,66 +1,39 @@
 // pages/cart/cart.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    address: {}
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onShow () {
+    const address = wx.getStorageSync('address')
+    this.setData({
+      address
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  // ----------- 事件处理函数 ------------
+  handleChooseAddress () {
+    wx.getSetting({
+      success: (res) => {
+        const addressAuth = res.authSetting['scope.address']
+        if (addressAuth === true || addressAuth === undefined) {
+          wx.chooseAddress({
+            success: (result) => {
+              wx.setStorageSync('address', result)
+            }
+          })
+        } else {
+          wx.openSetting({
+            success: () => {
+              // 成功授予权限
+              wx.chooseAddress({
+                success: (result) => {
+                  wx.setStorageSync('address', result)
+                }
+              })
+            }
+          })
+        }
+      }
+    })
 
   }
 })
--- pages/goods_detail/goods_detail.js
+++ pages/goods_detail/goods_detail.js
@@ -5,6 +5,7 @@ Page({
     goodsId: '',
     goodsInfo: {}
   },
+  GoodsInfo: {},
   onLoad: function (options) {
     const { goods_id } = options
     this.setData({
@@ -12,19 +13,50 @@ Page({
     })
     this._getGoodsDetailData()
   },
-  // 网络请求处理函数
+  // -------------- 网络请求处理函数 ---------
   _getGoodsDetailData () {
     const data = {
       goods_id: this.data.goodsId
     }
     getGoodsDetailData(data).then(res => {
       const goodsInfo = res.data.message
+      this.GoodsInfo = goodsInfo
       this.setData({
-        goodsInfo
+        goodsInfo: {
+          goods_name: goodsInfo.goods_name,
+          goods_price: goodsInfo.goods_price,
+          goods_introduce: goodsInfo.goods_introduce.replace(/\.webp/g, '.jpg'),
+          pics: goodsInfo.pics
+        }
       })
     })
     .catch(err => {
       console.log('商品详情信息获取失败', err)
+    })
+  },
+  // -------------- 事件处理函数 ---------
+  handleImgPreview (e) {
+    const urls = this.data.goodsInfo.pics.map(v => v.pics_mid)
+    const current = e.currentTarget.dataset.url
+    wx.previewImage({
+      current,
+      urls
+    })
+  },
+  handleAddCart () {
+    const cart = wx.getStorageSync('cart') || []
+    let index = cart.findIndex(i => i.goods_id === this.GoodsInfo.goods_id)
+    if (index === -1) {
+      this.GoodsInfo.num = 1
+      cart.push(this.GoodsInfo)
+    } else {
+      cart[index].num++
+    }
+    wx.setStorageSync("cart", cart)
+    wx.showToast({
+      title: '成功加入购物车',
+      icon: 'none',
+      mask: true
     })
   }
 })
