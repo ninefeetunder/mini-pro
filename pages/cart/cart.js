324e46745ac56118e5f80c629589b0ca588a4c00
ninefeetunder <1285499453@qq.com>
2019-12-10 21:39:05 +0800

    构建支付页面

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -38,6 +38,7 @@ Page({
     })
 
   },
+  // 单个商品选择处理
   handleCheckedChange (e) {
     // console.log(e.currentTarget.dataset.goodsid)
     const goodsId = e.currentTarget.dataset.goodsid
@@ -54,11 +55,63 @@ Page({
     wx.setStorageSync('cart', cart)
     this.getCartData()
   },
+  //  商品全选与反选
   handleAllChoose () {
     const cart = this.data.cart
+    let allChecked = this.data.allChecked
+    if (!allChecked) {
+      cart.forEach(v => {
+        if (!v.checked) {
+          v.checked = true
+        }
+      })
+    } else {
+      cart.forEach(v => {
+        v.checked = false
+      })
+    }
+    allChecked = !allChecked
+    this.setData({
+      cart,
+      allChecked
+    })
+    wx.setStorageSync('cart', cart)
+    this.getCartData()
+  },
+  handleDelGoods (e) {
+    const goodsId = e.currentTarget.dataset.goodsid
+    const cart = this.data.cart
+    const index = cart.findIndex(v => v.goods_id === goodsId)
+    if (cart[index].num === 1) {
+      wx.showModal({
+        title: '提示',
+        content: '删除该商品？',
+        success: (res) => {
+          if (res.confirm) {
+            cart.splice(index, 1)
+            this.setData({
+              cart
+            })
+            wx.setStorageSync('cart', cart)
+            this.getCartData()
+          }
+        }
+      })
+    } else {
+      cart[index].num--
+    }
+    this.setData({
+      cart
+    })
+    wx.setStorageSync('cart', cart)
+    this.getCartData()
+  },
+  handleAddGoods (e) {
+    const goodsId = e.currentTarget.dataset.goodsid
+    const cart = this.data.cart
     cart.forEach(v => {
-      if (!v.checked) {
-        v.checked = true
+      if (v.goods_id === goodsId) {
+        v.num++
       }
     })
     this.setData({
@@ -66,6 +119,26 @@ Page({
     })
     wx.setStorageSync('cart', cart)
     this.getCartData()
+  },
+  handlePay () {
+    const {address, totalNum} = this.data
+    if (!address.userName) {
+      wx.showToast({
+        title: '请选择地址',
+        icon: 'none'
+      })
+      return
+    }
+    if (!totalNum) {
+      wx.showToast({
+        title: '购物车里没有商品哦~',
+        icon: 'none'
+      })
+      return
+    }
+    wx.navigateTo({
+      url: '/pages/pay/pay'
+    })
   },
   getCartData () {
     const cart = wx.getStorageSync('cart') || []
